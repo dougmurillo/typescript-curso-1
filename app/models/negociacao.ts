fb8d2b3709345a7e800bf09e8512593d13b48af7
Douglas <douglas.murillo94@gmail.com>
2022-02-11 18:58:45 -0300

    first commit

---
+++ app/models/negociacao.ts
@@ -0,0 +1,18 @@
+export class Negociacao {
+   
+    constructor(
+        private _data: Date,
+        private readonly quantidade: number,
+        private readonly valor: number) {}
+
+    
+
+    get volume():number {
+        return this.quantidade * this.valor;
+    }
+
+    get data(): Date {
+        const data = new Date(this._data.getTime());
+        return this._data;
+    }
+}
